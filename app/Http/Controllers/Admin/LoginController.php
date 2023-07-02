f81ff0ebf2568c4f6514cff1da18ed9724339f2e
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2023-07-02 14:47:40 +0300

    v1.0.2

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -1,30 +1,32 @@
 <?php
-//لاتنسونا من صالح الدعاء
-//أخي الكريم هذا الكود هو اول 130 ساعة بالكورس الي نهاية الدورة الفيدو رقم  231- اما باقي أكواد الدورة الثانية للتطوير النظام موجوده بالدورة ولابد ان تكتبها بنفسك لأهميتها وللإستفادة
 
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
+
 class LoginController extends Controller
 {
-public function show_login_view(){
-return view('admin.auth.login');
-}
-public function login(LoginRequest $request){
-if(auth()->guard('admin')->attempt(['username'=>$request->input('username'),'password'=>$request->input('password')]))
-{
-return redirect()->route('dashboard');
-}else{
+    public function show_login_view()
+    {
+        return view('admin.auth.login');
+    }
+    public function login(LoginRequest $request)
+    {
+        if (auth()->guard('admin')->attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
+            return redirect()->route('dashboard');
+        } else {
 
-return redirect()->route('showlogin')->with(['error' => 'عفوا بيانات تسجيل الدخول غير صحيحة !!']);;
-}
-}
-public function logout(){
-auth()->logout();
-return redirect()->route('showlogin');
-}
-/*
+            return redirect()->route('showlogin')->with(['error' => 'عفوا بيانات تسجيل الدخول غير صحيحة !!']);;
+        }
+    }
+    public function logout()
+    {
+        auth()->logout();
+        return redirect()->route('showlogin');
+    }
+    /*
 function make_new_admin(){
 $admin=new App\Models\Admin();
 $admin->name='admin';
